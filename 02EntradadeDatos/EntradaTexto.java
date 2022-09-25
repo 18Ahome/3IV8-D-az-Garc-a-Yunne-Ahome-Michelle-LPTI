9aab224f9b02d98ac48baa5fba66ba9213a0638d
18Ahome <112513450+18Ahome@users.noreply.github.com>
2022-09-24 22:26:42 -0500

    Add files via upload

---
+++ 02EntradadeDatos/EntradaTexto.java
@@ -0,0 +1,33 @@
+//esto es un comentario
+/**
+ * Todo programa en java debe tener el nombre de la clase tal cual como el
+ * archivo
+ */
+
+class Entradadetexto {
+   // los limites de la clase
+
+   // toda clase debe de tener un metodo principal
+
+   /**
+    * Vamo a realizar un programa mediante el cual podemos introducir texto a
+    * nuestra conveniencia
+    */
+
+   // tipo de dato
+
+   // el ; es el fin de linea
+
+   // metodo principal
+
+   public static void main(String[] args) {
+      String nombre;
+      // instancia
+      System.out.println("Por favor introduce tu nombre asi bien kawaii");
+
+      // vamos a obtener el nombre del usuario
+
+      nombre = System.console().readLine();
+      System.out.println("Hola " + nombre + " bienvenido a tu programita");
+   }
+}
